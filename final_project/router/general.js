2fe4e52e5be11783084f9a7c44e2f5e8da3ea43e
NNGuide <nati9112.na@gmail.com>
2026-02-03 12:59:00 -0500

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,7 +67,12 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the ISBN parameter from the request URL and send the corresponding book's review
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  res.send(book.reviews);
+w
 });
 
 module.exports.general = public_users;
